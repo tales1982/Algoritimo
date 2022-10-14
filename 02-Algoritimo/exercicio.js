4dd98eb3ba5d84c1c443d4e12c8a7f69bd904dc6
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-10-15 00:03:19 +0200

    dados do cliente

---
+++ 02-Algoritimo/exercicio.js
@@ -0,0 +1,10 @@
+function cliente() {
+  let nome = prompt("Digite o nome do fucionario.");
+  let nacimento = prompt("Digite o ano de naçimento do fucionario.");
+  let salario = prompt("Digite o salario do fucionario.");
+  alert(`-------FICHA FUNCIONAL-------\n
+    NOME: ${nome}\n
+    NAÇIMENTO: ${nacimento}\n
+    SALARIO: ${salario}`);
+}
+cliente();
