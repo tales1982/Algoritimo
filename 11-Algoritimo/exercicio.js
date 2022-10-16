82458eadfef194cffccc5a58078b49ca78276fa8
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-10-16 23:00:10 +0200

    Em que cidade voçe mora?

--- 11-Algoritimo/exercicio.js
+++ 11-Algoritimo/exercicio.js
@@ -1,10 +1,11 @@
 let cidade = prompt("Em que cidade voçe mora?")
 let letraTotal = cidade.length
 alert(
-    `------------------RESULTADO---------------------
+  `------------------RESULTADO---------------------
     Voce mora na cidade: ${cidade}
-     A primeira letra é: ${cidade.substring(0,1)}    
+     A primeira letra é: ${cidade.charAt(0)}    
      E contem ${letraTotal} Letras
-     `)
+     `
+);
 
 
