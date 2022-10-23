e68069bad03a8d917ddb1a87ca653895cc52c6f1
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-10-23 09:16:06 +0200

    ano bissexto

--- 17-Algoritimo/index.js
+++ 17-Algoritimo/index.js
@@ -41,7 +41,7 @@ console.log(teste3);
 let teste4 = teste1 % 400
 console.log(teste4);
 if (teste2 === 0 && teste3!== 0 ||teste4 === 0){
-  alert("O ano é um ano bissexto (tem 366 dias).");
+  alert("O ano é um ano bissexto (tem 366 dias)..");
 }
 else{
   alert("O ano não é um ano bissexto (tem 365 dias).");
