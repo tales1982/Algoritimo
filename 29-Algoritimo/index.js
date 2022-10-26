0543054d6e5625ff3feaae7293f59827c690c45d
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-10-26 23:39:40 +0200

    contador

---
+++ 29-Algoritimo/index.js
@@ -0,0 +1,10 @@
+
+    let i = Number(prompt("Digite onde comesa a contagem."))
+    let f = Number(prompt('Digite onde termina a contagem'))
+    let p = Number(prompt("Qual vai ser o incremento"));
+    let c = 0
+
+while(c < f){
+    c += p
+    console.log(c + " ")
+}
