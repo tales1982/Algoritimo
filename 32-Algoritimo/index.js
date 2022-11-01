b717dd398cc23ba790ee6f67110a34d038a5f7e2
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-01 22:44:29 +0100

    exercicio 31

---
+++ 32-Algoritimo/index.js
@@ -0,0 +1,16 @@
+//Fazer uma contagem regressiva e marca os multiplos de [5]
+let n1 = 30
+let i = 1
+let c =4
+
+while(i <= n1){
+  
+  if(i %c == 0){
+    console.log(`[${i}]`)
+    
+}else{
+  console.log(i)
+}
+i++
+  }
+  
