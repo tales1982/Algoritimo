ad1a23f685b4a446dbdd462e0e98a269c1b571fa
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-15 22:14:09 +0100

    Numeros aleatorios

---
+++ 33-Algoritimo/index.js
@@ -0,0 +1,15 @@
+
+let numero = 10
+let c = 1
+let soma = 0
+
+
+while(c <= numero){
+  num = Math.random() * numero
+  soma += num
+ console.log(num.toFixed(0))
+this.resultado = soma
+  c++
+
+}
+console.log("A soma é "+ this.resultado.toFixed(0));
